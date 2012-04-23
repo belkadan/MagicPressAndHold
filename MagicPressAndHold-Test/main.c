bc2f05df87cbd779ab26df565e949050ee0d7667
Jordy Rose <jediknil@belkadan.com>
2012-04-23 17:25:21 -0400

    Keep around this example of how to get the currently enabled keyboard input sources.

--- MagicPressAndHold-Test/main.c
+++ MagicPressAndHold-Test/main.c
@@ -1,12 +1,23 @@
 #import <CoreFoundation/CoreFoundation.h>
 #import <Carbon/Carbon.h>
 
+void showName(TISInputSourceRef keyboard, void *unused) {
+	CFShow(TISGetInputSourceProperty(keyboard, kTISPropertyInputSourceID));
+}
+
+#define LENGTH(x) (sizeof(x)/sizeof(*(x)))
+
 int main(int argc, const char * argv[])
 {
-	TISInputSourceRef currentKeyboard = TISCopyCurrentKeyboardLayoutInputSource();
-	CFTypeRef type = TISGetInputSourceProperty(currentKeyboard, CFSTR("ComBelkadanTest"));
-	CFShow(type);
-	CFRelease(currentKeyboard);
+	CFTypeRef keys[] = { kTISPropertyInputSourceCategory, kTISPropertyInputSourceIsSelectCapable };
+	CFTypeRef values[] = { kTISCategoryKeyboardInputSource, kCFBooleanTrue };
+	CFDictionaryRef keyLayoutsOnly = CFDictionaryCreate(NULL, keys, values, LENGTH(keys), &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+
+	CFArrayRef inputSources = TISCreateInputSourceList(keyLayoutsOnly, false);
+	CFArrayApplyFunction(inputSources, CFRangeMake(0, CFArrayGetCount(inputSources)), (CFArrayApplierFunction)&showName, NULL);
+
+	CFRelease(inputSources);
+	CFRelease(keyLayoutsOnly);
     return 0;
 }
 
