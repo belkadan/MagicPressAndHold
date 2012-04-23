9cdcc02d1590264f7af9c0bb3ec3abba62746145
Jordy Rose <jediknil@belkadan.com>
2012-04-23 14:25:44 -0400

    Log pressed keys. Note that this does not actually work unless you manually load SIMBL plugins for PressAndHold.

--- MagicPressAndHold/Server.m
+++ MagicPressAndHold/Server.m
@@ -7,12 +7,30 @@
 //
 
 #import "Server.h"
+#import	"SwizzleMacros.h"
+#import "PressAndHold.h"
 
 @implementation Server
 
 + (void)load
 {
-	
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		Class PAHInputController_class = NSClassFromString(@"PAHInputController");
+		if (!PAHInputController_class) return;
+
+		COPY_AND_EXCHANGE(self, PAHInputController_class, ComBelkadanPressAndHold_, accentsStringForString:language:);
+	});
+}
+
+#pragma mark -
+
+- (NSString *)ComBelkadanPressAndHold_accentsStringForString:(NSString *)string language:(NSString *)language
+{
+	NSLog(@"%@ (%@ / %@)", string, language, [language class]);
+	id result = [self ComBelkadanPressAndHold_accentsStringForString:string language:language];
+	NSLog(@"-> %@ (%@)", result, [result class]);
+	return result;
 }
 
 @end
