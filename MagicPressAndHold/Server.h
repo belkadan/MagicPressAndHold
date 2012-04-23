c5e6b32ed0809e37c7265c2d7f361d1c389ff16c
Jordy Rose <jediknil@belkadan.com>
2012-04-23 13:19:51 -0400

    SIMBLify.

---
+++ MagicPressAndHold/Server.h
@@ -0,0 +1,15 @@
+//
+//  Server.h
+//  MagicPressAndHold
+//
+//  Created by Jordy Rose on 2012-04-23.
+//  Copyright (c) 2012 Belkadan Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ComBelkadanPressAndHold_Server : NSObject
+
+@end
+
+@compatibility_alias Server ComBelkadanPressAndHold_Server;
---
+++ MagicPressAndHold/Server.m
@@ -0,0 +1,18 @@
+//
+//  Server.m
+//  MagicPressAndHold
+//
+//  Created by Jordy Rose on 2012-04-23.
+//  Copyright (c) 2012 Belkadan Software. All rights reserved.
+//
+
+#import "Server.h"
+
+@implementation Server
+
++ (void)load
+{
+	
+}
+
+@end
